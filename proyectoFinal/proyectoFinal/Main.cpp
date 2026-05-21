8508d663450c3736c41d70b9d06add281b037707
crissalar <cris.silv.14@gmail.com>
2026-05-20 21:09:02 -0600

    Adición de nuevas animaciones y corrección de música implementada [TEORÍA]

--- proyectoFinal/proyectoFinal/Main.cpp
+++ proyectoFinal/proyectoFinal/Main.cpp
@@ -1,6 +1,6 @@
 ﻿/*
 *Proyecto Final
-*Fecha de Entrega: 13/05/26
+*Fecha de Entrega: 20/05/26
 *319271108
 */
 
@@ -8,22 +8,16 @@
 #include <stdlib.h>
 #include <vector>
 #include <string>
-#include <cmath> // Para std::abs
+#include <cmath>
 
-// GLAD
 #include <glad/glad.h>
-
-// GLFW
 #include <GLFW/glfw3.h>
-
-// GLM
 #include <glm/glm.hpp>
 #include <glm/gtc/matrix_transform.hpp>
 #include <glm/gtc/type_ptr.hpp>
 #include <irrKlang.h>
 using namespace irrklang;
 
-// Model loading classes
 #include <shader_m.h>
 #include <camera.h>
 #include <model.h>
@@ -31,51 +25,56 @@ using namespace irrklang;
 #include <light.h>
 #include <cubemap.h>
 
-// Functions
 bool Start();
 bool Update();
 void InitSounds();
 
-// Helpers de Iluminación
 void SetLightUniformInt(Shader* shader, const char* propertyName, size_t lightIndex, int value);
 void SetLightUniformFloat(Shader* shader, const char* propertyName, size_t lightIndex, float value);
 void SetLightUniformVec4(Shader* shader, const char* propertyName, size_t lightIndex, glm::vec4 value);
 void SetLightUniformVec3(Shader* shader, const char* propertyName, size_t lightIndex, glm::vec3 value);
 
-// Callbacks
 void framebuffer_size_callback(GLFWwindow* window, int width, int height);
 void mouse_callback(GLFWwindow* window, double xpos, double ypos);
 void scroll_callback(GLFWwindow* window, double xoffset, double yoffset);
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods);
 void processInput(GLFWwindow* window);
 
-// Globals
 GLFWwindow* window;
 
-// Window size
 const unsigned int SCR_WIDTH = 1024;
 const unsigned int SCR_HEIGHT = 768;
 
-// Camera (Posición inicial cerca del policía, ligeramente elevada)
 Camera camera(glm::vec3(16.0f, 8.98f, 2.5f));
 
-// Mouse
 float lastX = SCR_WIDTH / 2.0f;
 float lastY = SCR_HEIGHT / 2.0f;
 bool firstMouse = true;
+bool mouseCapturado = false;
 
-// Time
 float deltaTime = 0.0f;
 float lastFrame = 0.0f;
 
-// Variables de Estado (Día/Noche, Cámara, TP)
 bool esDia = true;
 bool freeCamera = false;
-float fixedCameraY = 8.98f; // Altura fija para el modo piso
+float fixedCameraY = 8.98f;
 
 bool keyTPressed = false;
 bool keyCPressed = false;
 bool key1Pressed = false;
 bool key2Pressed = false;
+
+bool keyFPressed = false;
+bool puertaAbierta = false;
+float puertaRotacionActual = 0.0f;
+float puertaRotacionObjetivo = 0.0f;
+
+AnimatedModel* visitante;
+glm::vec3 visitante_pos_inicial(-15.0f, 6.98f, 0.0f);
+glm::vec3 visitante_pos_final(10.0f, 6.98f, 0.0f);
+glm::vec3 visitante_position = visitante_pos_inicial;
+float visitante_velocidad = 2.0f;
+bool visitante_va_hacia_adelante = true;
 
 #pragma region Transformaciones
 glm::vec3 expositor1_position(-1.0f, 6.98f, -1.0f);
@@ -85,33 +84,31 @@ glm::vec3 expositor4_position(6.5f, 6.98f, 1.4f);
 glm::vec3 policia_position(16.0f, 6.98f, 0.5f);
 glm::vec3 miku_position(-1.0f, 14.8f, -1.0f);
 
+glm::vec3 scale_puerta(1.0f, 1.0f, 1.0f);
 glm::vec3 scale_expositors(0.012f, 0.012f, 0.012f);
-glm::vec3 scale_expositors_small(0.0012f, 0.0012f, 0.0012f);
+glm::vec3 scale_expositors_small(0.002f, 0.002f, 0.002f);
 glm::vec3 scale_expositors_xs(0.0009f, 0.0009f, 0.0009f);
 glm::vec3 scale_miku(0.0005f, 0.0005f, 0.0005f);
+glm::vec3 scale_visitante(0.005f, 0.005f, 0.005f);
 #pragma endregion
 
-// Shaders
 Shader* phongShader;
 Shader* dynamicShader;
 Shader* cubemapShader;
 
-// Static models
 Model* house;
 Model* escenario;
+Model* puerta;
 
-// Animated models
 AnimatedModel* expositor1;
 AnimatedModel* expositor2;
 AnimatedModel* expositor3;
 AnimatedModel* policia;
 AnimatedModel* miku;
 
-// Audio
 ISoundEngine* SoundEngine = createIrrKlangDevice();
 ISound* musicaFondo = nullptr;
 
-// Cubemaps
 CubeMap* skyboxdia = nullptr;
 CubeMap* skyboxnoche = nullptr;
 
@@ -151,7 +148,9 @@ bool Start()
     glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
     glfwSetCursorPosCallback(window, mouse_callback);
     glfwSetScrollCallback(window, scroll_callback);
-    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+    glfwSetMouseButtonCallback(window, mouse_button_callback);
+
+    glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
 
     if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress))
     {
@@ -167,17 +166,17 @@ bool Start()
 
     dynamicShader->setBonesIDs(MAX_RIGGING_BONES);
 
-    // Load models
     house = new Model("models/Lobby/lobby.obj");
     escenario = new Model("models/Lobby/escenario.obj");
+    puerta = new Model("models/Lobby/puerta.obj");
 
     miku = new AnimatedModel("models/Lobby/miku.fbx");
+    visitante = new AnimatedModel("models/Lobby/kid_walking.fbx");
     expositor1 = new AnimatedModel("models/Lobby/expositor_1.fbx");
     expositor2 = new AnimatedModel("models/Lobby/expositor_2.fbx");
     expositor3 = new AnimatedModel("models/Lobby/expositor_3.fbx");
     policia = new AnimatedModel("models/Lobby/poli.fbx");
 
-    // Load cubemap DÍA
     std::vector<std::string> dayFaces = {
         "cubemap/dia/px.png", "cubemap/dia/nx.png",
         "cubemap/dia/py.png", "cubemap/dia/ny.png",
@@ -186,7 +185,6 @@ bool Start()
     skyboxdia = new CubeMap();
     skyboxdia->loadCubemap(dayFaces);
 
-    // Load cubemap NOCHE 
     std::vector<std::string> nightFaces = {
         "cubemap/noche/px.png", "cubemap/noche/nx.png",
         "cubemap/noche/py.png", "cubemap/noche/ny.png",
@@ -200,7 +198,6 @@ bool Start()
 
 void InitSounds()
 {
-    // Iniciamos la música pausada por defecto
     musicaFondo = SoundEngine->play2D("sound/bosque-120184.mp3", true, true, true);
 }
 
@@ -212,22 +209,56 @@ bool Update()
 
     processInput(window);
 
-    // --- SISTEMA DE AUDIO POR RANGO (MIKU) ---
-    float distanciaXMiku = std::abs(camera.Position.x - miku_position.x);
-    float distanciaZMiku = std::abs(camera.Position.z - miku_position.z);
+    // Lógica Puerta
+    float rotSpeed = 5.0f;
+    puertaRotacionActual += (puertaRotacionObjetivo - puertaRotacionActual) * rotSpeed * deltaTime;
 
-    if (distanciaXMiku <= 10.0f && distanciaZMiku <= 10.0f) {
-        if (musicaFondo && musicaFondo->getIsPaused()) {
+    // Audio dinámico
+    float distanciaMiku = glm::length(camera.Position - miku_position);
+    float distanciaMaxima = 20.0f;
+    float distanciaOptima = 5.0f;
+
+    if (camera.Position.y < 11.0f || distanciaMiku > distanciaMaxima) {
+        if (musicaFondo && !musicaFondo->getIsPaused())
+            musicaFondo->setIsPaused(true);
+    }
+    else {
+        if (musicaFondo && musicaFondo->getIsPaused())
             musicaFondo->setIsPaused(false);
+
+        if (distanciaMiku <= distanciaOptima) {
+            musicaFondo->setVolume(1.0f);
+        }
+        else {
+            float rangoAtenuacion = distanciaMaxima - distanciaOptima;
+            float distanciaExcedente = distanciaMiku - distanciaOptima;
+            float volumenCalculado = 1.0f - (distanciaExcedente / rangoAtenuacion);
+            if (volumenCalculado < 0.0f) volumenCalculado = 0.0f;
+            musicaFondo->setVolume(volumenCalculado);
+        }
+    }
+
+    // Visitante
+    visitante->UpdateAnimation(deltaTime);
+    glm::vec3 direccion = glm::normalize(visitante_pos_final - visitante_pos_inicial);
+    float distanciaTotal = glm::length(visitante_pos_final - visitante_pos_inicial);
+
+    if (visitante_va_hacia_adelante) {
+        visitante_position += direccion * visitante_velocidad * deltaTime;
+        if (glm::length(visitante_position - visitante_pos_inicial) >= distanciaTotal) {
+            visitante_position = visitante_pos_final;
+            visitante_va_hacia_adelante = false;
         }
     }
     else {
-        if (musicaFondo && !musicaFondo->getIsPaused()) {
-            musicaFondo->setIsPaused(true);
+        visitante_position -= direccion * visitante_velocidad * deltaTime;
+        if (glm::length(visitante_position - visitante_pos_final) >= distanciaTotal) {
+            visitante_position = visitante_pos_inicial;
+            visitante_va_hacia_adelante = true;
         }
     }
-    // ------------------------------------------
 
+    // Renderizado
     glClearColor(0.2f, 0.2f, 0.2f, 1.0f);
     glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
@@ -242,7 +273,7 @@ bool Update()
     }
     glUseProgram(0);
 
-    // Static objects (Afectados por luz)
+    // Static objects
     {
         phongShader->use();
         glEnable(GL_BLEND);
@@ -262,8 +293,6 @@ bool Update()
             SetLightUniformVec3(phongShader, "Position", 0, glm::vec3(0.0f, 150.0f, 0.0f));
             SetLightUniformVec3(phongShader, "Direction", 0, glm::vec3(0.0f, -1.0f, 0.0f));
             SetLightUniformVec4(phongShader, "Color", 0, glm::vec4(1.0f, 1.0f, 1.0f, 1.0f));
-
-            // CORRECCIÓN MAGNA: Power más bajo y distance en 1.0f
             SetLightUniformVec4(phongShader, "Power", 0, glm::vec4(1.5f, 1.5f, 1.5f, 1.0f));
             SetLightUniformInt(phongShader, "alphaIndex", 0, 100);
             SetLightUniformFloat(phongShader, "distance", 0, 1.0f);
@@ -271,50 +300,56 @@ bool Update()
         else {
             phongShader->setInt("numLights", 4);
             glm::vec4 warmColor(1.0f, 0.7f, 0.3f, 1.0f);
-
-            // CORRECCIÓN MAGNA: Power ajustado
             glm::vec4 warmPower(1.2f, 1.2f, 1.2f, 1.0f);
 
-            // Luz 1
             SetLightUniformVec3(phongShader, "Position", 0, glm::vec3(-5.0f, 5.0f, -5.0f));
             SetLightUniformVec4(phongShader, "Color", 0, warmColor);
             SetLightUniformVec4(phongShader, "Power", 0, warmPower);
-            SetLightUniformFloat(phongShader, "distance", 0, 1.0f); // distance a 1.0f
+            SetLightUniformFloat(phongShader, "distance", 0, 1.0f);
             SetLightUniformInt(phongShader, "alphaIndex", 0, 10);
 
-            // Luz 2
             SetLightUniformVec3(phongShader, "Position", 1, glm::vec3(5.0f, 5.0f, -5.0f));
             SetLightUniformVec4(phongShader, "Color", 1, warmColor);
             SetLightUniformVec4(phongShader, "Power", 1, warmPower);
-            SetLightUniformFloat(phongShader, "distance", 1, 1.0f); // distance a 1.0f
+            SetLightUniformFloat(phongShader, "distance", 1, 1.0f);
             SetLightUniformInt(phongShader, "alphaIndex", 1, 10);
 
-            // Luz 3
             SetLightUniformVec3(phongShader, "Position", 2, glm::vec3(-5.0f, 5.0f, 5.0f));
             SetLightUniformVec4(phongShader, "Color", 2, warmColor);
             SetLightUniformVec4(phongShader, "Power", 2, warmPower);
-            SetLightUniformFloat(phongShader, "distance", 2, 1.0f); // distance a 1.0f
+            SetLightUniformFloat(phongShader, "distance", 2, 1.0f);
             SetLightUniformInt(phongShader, "alphaIndex", 2, 10);
 
-            // Luz 4
             SetLightUniformVec3(phongShader, "Position", 3, glm::vec3(5.0f, 5.0f, 5.0f));
             SetLightUniformVec4(phongShader, "Color", 3, warmColor);
             SetLightUniformVec4(phongShader, "Power", 3, warmPower);
-            SetLightUniformFloat(phongShader, "distance", 3, 1.0f); // distance a 1.0f
+            SetLightUniformFloat(phongShader, "distance", 3, 1.0f);
             SetLightUniformInt(phongShader, "alphaIndex", 3, 10);
         }
 
+        // Lobby
         glm::mat4 model = glm::mat4(1.0f);
         phongShader->setMat4("model", model);
         house->Draw(*phongShader);
 
+        // Escenario
         glm::mat4 modelEscenario = glm::mat4(1.0f);
         phongShader->setMat4("model", modelEscenario);
         escenario->Draw(*phongShader);
+
+        // Puerta
+        glm::vec3 pivoteBisagra(19.957f, 13.948f, -3.5561f);
+        glm::mat4 modelPuerta = glm::mat4(1.0f);
+        modelPuerta = glm::translate(modelPuerta, pivoteBisagra);
+        modelPuerta = glm::rotate(modelPuerta, glm::radians(puertaRotacionActual), glm::vec3(0.0f, 1.0f, 0.0f));
+        modelPuerta = glm::translate(modelPuerta, -pivoteBisagra);
+        modelPuerta = glm::scale(modelPuerta, scale_puerta);
+        phongShader->setMat4("model", modelPuerta);
+        puerta->Draw(*phongShader);
     }
     glUseProgram(0);
 
-    // Miku 
+    // Miku
     {
         miku->UpdateAnimation(deltaTime);
         dynamicShader->use();
@@ -328,6 +363,23 @@ bool Update()
         dynamicShader->setMat4("model", model);
         dynamicShader->setMat4("gBones", MAX_RIGGING_BONES, miku->gBones);
         miku->Draw(*dynamicShader);
+    }
+    glUseProgram(0);
+
+    // Visitante
+    {
+        dynamicShader->use();
+        dynamicShader->setMat4("projection", projection);
+        dynamicShader->setMat4("view", view);
+
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::translate(model, visitante_position);
+        float anguloRotacion = visitante_va_hacia_adelante ? 90.0f : -90.0f;
+        model = glm::rotate(model, glm::radians(anguloRotacion), glm::vec3(0.0f, 1.0f, 0.0f));
+        model = glm::scale(model, scale_visitante);
+        dynamicShader->setMat4("model", model);
+        dynamicShader->setMat4("gBones", MAX_RIGGING_BONES, visitante->gBones);
+        visitante->Draw(*dynamicShader);
     }
     glUseProgram(0);
 
@@ -406,10 +458,28 @@ bool Update()
 
 void processInput(GLFWwindow* window)
 {
-    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
-        glfwSetWindowShouldClose(window, true);
+    // ESC: liberar mouse o cerrar ventana
+    if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+        if (mouseCapturado) {
+            mouseCapturado = false;
+            glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_NORMAL);
+        }
+        else {
+            glfwSetWindowShouldClose(window, true);
+        }
+    }
 
-    // Controles de Modo (Día/Noche con T)
+    // ✅ F - Puerta (ANTES del guard)
+    if (glfwGetKey(window, GLFW_KEY_F) == GLFW_PRESS && !keyFPressed) {
+        puertaAbierta = !puertaAbierta;
+        puertaRotacionObjetivo = puertaAbierta ? -90.0f : 0.0f;
+        keyFPressed = true;
+    }
+    if (glfwGetKey(window, GLFW_KEY_F) == GLFW_RELEASE) {
+        keyFPressed = false;
+    }
+
+    // ✅ T - Día/Noche (ANTES del guard)
     if (glfwGetKey(window, GLFW_KEY_T) == GLFW_PRESS && !keyTPressed) {
         esDia = !esDia;
         keyTPressed = true;
@@ -418,7 +488,7 @@ void processInput(GLFWwindow* window)
         keyTPressed = false;
     }
 
-    // Controles de Cámara (Libre/Piso con C)
+    // ✅ C - Cámara libre/fija (ANTES del guard)
     if (glfwGetKey(window, GLFW_KEY_C) == GLFW_PRESS && !keyCPressed) {
         freeCamera = !freeCamera;
         if (!freeCamera) fixedCameraY = camera.Position.y;
@@ -428,7 +498,10 @@ void processInput(GLFWwindow* window)
         keyCPressed = false;
     }
 
-    // Teletransporte a Miku (Tecla 1)
+    // Guard: si el mouse no está capturado, no procesar movimiento
+    if (!mouseCapturado) return;
+
+    // TP a Miku (Tecla 1)
     if (glfwGetKey(window, GLFW_KEY_1) == GLFW_PRESS && !key1Pressed) {
         camera.Position = glm::vec3(miku_position.x, miku_position.y + 1.0f, miku_position.z + 2.5f);
         fixedCameraY = camera.Position.y;
@@ -438,7 +511,7 @@ void processInput(GLFWwindow* window)
         key1Pressed = false;
     }
 
-    // Teletransporte al Policía (Tecla 2)
+    // TP al Policía (Tecla 2)
     if (glfwGetKey(window, GLFW_KEY_2) == GLFW_PRESS && !key2Pressed) {
         camera.Position = glm::vec3(policia_position.x, policia_position.y + 2.0f, policia_position.z + 2.5f);
         fixedCameraY = camera.Position.y;
@@ -458,7 +531,7 @@ void processInput(GLFWwindow* window)
     if (glfwGetKey(window, GLFW_KEY_D) == GLFW_PRESS)
         camera.ProcessKeyboard(RIGHT, deltaTime);
 
-    // Controles de altura manual (Solo en modo libre)
+    // Altura (solo en modo libre)
     if (freeCamera) {
         if (glfwGetKey(window, GLFW_KEY_E) == GLFW_PRESS)
             camera.ProcessKeyboard(UP_DIR, deltaTime);
@@ -477,6 +550,18 @@ void processInput(GLFWwindow* window)
         glPolygonMode(GL_FRONT_AND_BACK, GL_POINT);
 }
 
+void mouse_button_callback(GLFWwindow* window, int button, int action, int mods)
+{
+    if (button == GLFW_MOUSE_BUTTON_LEFT && action == GLFW_PRESS)
+    {
+        if (!mouseCapturado) {
+            mouseCapturado = true;
+            glfwSetInputMode(window, GLFW_CURSOR, GLFW_CURSOR_DISABLED);
+            firstMouse = true;
+        }
+    }
+}
+
 void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 {
     glViewport(0, 0, width, height);
@@ -484,6 +569,8 @@ void framebuffer_size_callback(GLFWwindow* window, int width, int height)
 
 void mouse_callback(GLFWwindow* window, double xpos, double ypos)
 {
+    if (!mouseCapturado) return;
+
     if (firstMouse)
     {
         lastX = (float)xpos;
@@ -505,7 +592,6 @@ void scroll_callback(GLFWwindow* window, double xoffset, double yoffset)
     camera.ProcessMouseScroll((float)yoffset);
 }
 
-// Helpers Iluminación
 void SetLightUniformInt(Shader* shader, const char* propertyName, size_t lightIndex, int value) {
     std::string uniformName = "allLights[" + std::to_string(lightIndex) + "]." + propertyName;
     shader->setInt(uniformName.c_str(), value);
